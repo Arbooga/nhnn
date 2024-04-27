6d25215e59b3af36061f084a5e6495c84abf7e11
Arbooga <155502100+Arbooga@users.noreply.github.com>
2024-04-27 18:33:17 +0200

    Update gg.php

--- gg.php
+++ gg.php
@@ -15,7 +15,7 @@ $sql = "CREATE DATABASE my20";
 if ($conn->query($sql) === TRUE) {
   echo "Database created successfully";
 } else {
-  echo "Error creating database: " . $conn->e77rror;
+  echo "Error creating database: " . $conn->e7rror;
 }
 
 $conn->close();
